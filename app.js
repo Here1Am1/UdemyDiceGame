a01668f672a821d981945adbec65945323ded4b9
Josh Bell <blitz43p@gmail.com>
2019-02-10 00:26:03 -0600

    Added more rules and stipulations

--- app.js
+++ app.js
@@ -3,52 +3,81 @@ GAME RULES:
 
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he or she whishes. Each result gets added to his or her ROUND score
-- BUT, if the player rolls a 1, all ot their ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- BUT, if the player rolls a 1, all ot their ROUND score gets lost. If two sixes are rolled back to back, all of that players
+- round score and saved score is lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that their ROUND score gets added to their GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 
 var scrores, roundScores, activePlayer, dice, gamePlaying;
+var previousRoll;
 
 init();
-
-
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
   if (gamePlaying) {
 
-    // 1. Random number
+    // 1. Random numbers for dice
     var dice = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display the result (dice image)
     var diceDOM = document.querySelector('.dice');
+    var diceDOM2 = document.querySelector('.dice2');
 
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
-    // 3. Update the round score if the rolled number was not a 1
-    if (dice !== 1) {
+    diceDOM2.style.display = 'block';
+    diceDOM2.src = 'dice-' + dice2 + '.png';
+
+    // 3. Update the round score if one of the rolled numbers is not a 1, or if it is not two sixes
+    if ((dice === 6 && previousRollOne === 6) || (dice2 === 6 && previousRollOne === 6) || (dice === 6 && dice2 ===6)
+
+      || (dice === 6 && previousRollTwo === 6) || (dice2 === 6 && previousRollTwo === 6)) {
+
+      // player loses score
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = '0';
+
+      // alert message for when two sixes are rolled consecutively
+      alert('You have rolled two sixes in a row. You lose your total score');
+
+      // reset the dice and previousRoll varialbes for the next player's turn
+      dice = 0;
+      dice2 = 0;
+      previousRollOne = 0;
+      previousRollTwo = 0;
+
+      nextPlayer();
+
+    } else if (dice !== 1 && dice2 !== 1) {
 
       // Add score
-      roundScore += dice;
+      roundScore += dice + dice2;
+
+      // display active player's current round score
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
     } else {
 
       nextPlayer();
 
-    }
+    }// end of else for else if (dice !== 1)
 
-  }
+    // set previous roll varibles for when testing for consecutive sixes
+    previousRollOne = dice;
+    perviousRollTwo = dice2;
 
+  }// end of if (gamePlaying)
 
 })// end of document.querySelector('.btn-roll').addEventListener('click', function()
 
-
 document.querySelector('.btn-hold').addEventListener('click', function() {
 
+  // only run code if the game state variable (gamePlaying) is set
   if (gamePlaying) {
 
     // Add current scrore to global score
@@ -57,22 +86,42 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    // getting the value of the input field
+    var input = document.querySelector('.score-limit').value;
+
+    // variable used to hold the value from the input field
+    var winningScore;
+
+    // Undefined, 0, null, or "" are coerced to false, and anything else is coerced to true.
+    // check to see if the input variable is empty
+    if (input) {
+
+      winningScore = input;
+
+    } else {
+
+      winningScore = 100;
+
+    }
+
     // Check if the player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
 
       document.querySelector('#name-' + activePlayer).textContent = 'Winner';
       document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.dice2').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
       gamePlaying = false;
 
     } else {
 
       nextPlayer();
 
-    }
+    }// end of else for if (scores[activePlayer] >= winningScore)
 
-  }
+  }// end of if (gamePlaying)
 
 });// end of document.querySelector('.btn-hold').addEventListener('click', function() {
 
@@ -88,6 +137,7 @@ function nextPlayer() {
   document.querySelector('.player-1-panel').classList.toggle('active');
 
   document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.dice2').style.display = 'none';
 
 }// end of nextPlayer() function
 
@@ -98,10 +148,12 @@ function init() {
   scores = [0,0];
   activePlayer = 0;
   roundScore = 0;
-  gamePlaying = true;
+  gamePlaying = true
+
+  document.querySelector('.score-limit').value = '';
 
   document.querySelector('.dice').style.display = 'none';
-
+  document.querySelector('.dice2').style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
@@ -114,9 +166,4 @@ function init() {
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
 
-}
-
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
+}// end of init() function
